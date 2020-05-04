b20b6e690030784e0655fdb6e1352ccff62cefad
Eliška Brablcová <eliska.brablcova@gmail.com>
2020-05-04 14:59:03 +0200

    note entity

---
+++ Models/Entities/Note.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eli.TimeManagement.Models.Entities
+{
+	public class Note
+	{
+		public int ID { get; set; }
+		public string Text { get; set; }
+		public DateTime Edited { get; set; }
+	}
+}
