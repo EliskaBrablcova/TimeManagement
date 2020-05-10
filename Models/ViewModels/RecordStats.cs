5f5cccbcf4cf70ecd2bd6927b3b71eb64e937bf4
Eliška Brablcová <eliska.brablcova@gmail.com>
2020-05-10 16:58:40 +0200

    View Models for Stats

---
+++ Models/ViewModels/RecordStats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eli.TimeManagement.Models.ViewModels
+{
+	public class RecordStats
+	{
+		public int MinutesTotal { get; set; }
+		public int ActiveDaysTotal { get; set; }
+		public int DaysTotal { get; set; }
+		public double ActiveDaysPercent { get; set; }
+		public double MinutesPerActiveDays { get; set; }
+		public double MinutesPerDays { get; set; }
+		public List<TypeStats> TypesStats { get; set; }
+	}
+}
---
+++ Models/ViewModels/TypeStats.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eli.TimeManagement.Models.ViewModels
+{
+	public class TypeStats
+	{
+		public string Type { get; set; }
+		public double Percent { get; set; }
+	}
+}
