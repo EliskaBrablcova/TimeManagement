8697b3a6c1819ab4e10e5b181362eca5f238f35a
Eliška Brablcová <eliska.brablcova@gmail.com>
2020-05-25 13:30:06 +0200

    Check Item repository

---
+++ Repository/CheckItemFileRepository.cs
@@ -0,0 +1,141 @@
+﻿using Eli.TimeManagement.Models.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Eli.TimeManagement.Repository
+{
+	public class CheckItemFileRepository : ICheckItemRepository
+	{
+		private readonly string _fileName;
+
+		public CheckItemFileRepository(string fileName)
+		{
+			if (fileName == null)
+			{
+				throw new ArgumentNullException(nameof(fileName));
+			}
+			_fileName = fileName;
+		}
+
+		public void Add(CheckItem newCheckItem)
+		{
+			var items = readFromFile();
+			newCheckItem.ID = getId(items);
+			items.Add(newCheckItem);
+			saveToFile(items);
+		}
+
+		public void Complete(int id)
+		{
+			var items = readFromFile();
+			var position = getPosition(items, id);
+			if (position == null)
+			{
+				throw new ArgumentOutOfRangeException(nameof(id));
+			}
+			var item = items[position.Value];
+			item.Completed = true;
+			saveToFile(items);
+		}
+
+		public void Delete(int id)
+		{
+			var items = readFromFile();
+			var position = getPosition(items, id);
+			if (position == null)
+			{
+				throw new ArgumentOutOfRangeException(nameof(id));
+			}
+			items.RemoveAt(position.Value);
+			saveToFile(items);
+		}
+
+		public void Edit(CheckItem editCheckItem)
+		{
+			var items = readFromFile();
+			var position = getPosition(items, editCheckItem.ID);
+			if (position == null)
+			{
+				throw new ArgumentOutOfRangeException(nameof(editCheckItem));
+			}
+			var item = items[position.Value];
+			item.Type = editCheckItem.Type;
+			item.Text = editCheckItem.Text;
+			item.Completed = editCheckItem.Completed;
+			saveToFile(items);
+		}
+
+		public IList<CheckItem> GetAll(bool notCompletedOnly)
+		{
+			var items = readFromFileOrdered();
+			if (!notCompletedOnly)
+			{
+				return items;
+			}
+			var toReturn = new List<CheckItem>();
+			for (int i = 0; i < items.Count; i++)
+			{
+				var item = items[i];
+				if (!item.Completed)
+				{
+					toReturn.Add(item);
+				}
+			}
+			return toReturn;
+		}
+
+		private List<CheckItem> readFromFileOrdered()
+		{
+			var items = readFromFile();
+			return items.OrderBy(c => c.Type).ThenByDescending(c => c.Completed).ThenBy(c => c.ID).ToList();
+		}
+		private List<CheckItem> readFromFile()
+		{
+			//TODO toto Eli ještě neviděla
+			using (var sr = new StreamReader(_fileName))
+			{
+				return JsonConvert.DeserializeObject<List<CheckItem>>(sr.ReadToEnd());
+			}
+		}
+
+		private void saveToFile(List<CheckItem> items)
+		{
+			//TODO toto Eli ještě neviděla
+			using (var sw = new StreamWriter(_fileName))
+			{
+				sw.Write(JsonConvert.SerializeObject(items));
+			}
+		}
+
+		private int? getPosition(List<CheckItem> items, int id)
+		{
+			for (int i = 0; i < items.Count; i++)
+			{
+				var item = items[i];
+				if (item.ID == id)
+				{
+					return i;
+				}
+			}
+			return null;
+		}
+
+		private int getId(List<CheckItem> items)
+		{
+			var id = 0;
+			for (int i = 0; i < items.Count; i++)
+			{
+				var item = items[i];
+				if (item.ID > id)
+				{
+					id = item.ID;
+				}
+			}
+			return id + 1;
+		}
+	}
+}
---
+++ Repository/ICheckItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using Eli.TimeManagement.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eli.TimeManagement.Repository
+{
+	public interface ICheckItemRepository
+	{
+		IList<CheckItem> GetAll(bool notCompletedOnly);
+		void Add(CheckItem newCheckItem);
+		void Edit(CheckItem editCheckItem);
+		void Delete(int id);
+		void Complete(int id);
+	}
+}
