263082fadc32bd966f12b3d55ff3bb635cf05b3f
Vaclav Fajta <fajtavaclav@gmail.com>
2020-06-27 00:01:12 +0200

    nullable date time picker

---
+++ FormsLibrary/NullableDateTimePicker.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Eli.TimeManagement.FormsLibrary
+{
+	partial class NullableDateTimePicker
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dtp = new System.Windows.Forms.DateTimePicker();
+			this.SuspendLayout();
+			// 
+			// dtp
+			// 
+			this.dtp.CustomFormat = "dd.MM.yyyy";
+			this.dtp.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dtp.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+			this.dtp.Location = new System.Drawing.Point(0, 0);
+			this.dtp.Margin = new System.Windows.Forms.Padding(0);
+			this.dtp.Name = "dtp";
+			this.dtp.Size = new System.Drawing.Size(200, 20);
+			this.dtp.TabIndex = 0;
+			// 
+			// NullableDateTimePicker
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.dtp);
+			this.Name = "NullableDateTimePicker";
+			this.Size = new System.Drawing.Size(200, 20);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DateTimePicker dtp;
+	}
+}
---
+++ FormsLibrary/NullableDateTimePicker.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Eli.TimeManagement.FormsLibrary
+{
+	public partial class NullableDateTimePicker : UserControl
+	{
+		public NullableDateTimePicker()
+		{
+			InitializeComponent();
+			dtp.ValueChanged += Dtp_ValueChanged;
+			dtp.KeyDown += Dtp_KeyDown;
+		}
+
+		private DateTime? _value;
+		private bool _reactToValueChange = true;
+		private EventHandler _onValueChanged;
+		private string _customFormat;
+		private const string _nullFormat = " ";
+
+		public event EventHandler ValueChanged
+		{
+			add => _onValueChanged += value;
+			remove => _onValueChanged -= value;
+		}
+
+		[Bindable(true)]
+		[RefreshProperties(RefreshProperties.All)]
+		[DefaultValue(null)]
+		public DateTime? Value
+		{
+			get => _value;
+			set
+			{
+				var changed = value != _value;
+				_value = value;
+				if (value != null)
+				{
+					_reactToValueChange = false;
+					dtp.Value = value.Value;
+					dtp.CustomFormat = _customFormat;
+					_reactToValueChange = true;
+				}
+				else
+				{
+					dtp.CustomFormat = _nullFormat;
+				}
+
+				if (changed)
+				{
+					OnValueChanged(EventArgs.Empty);
+				}
+			}
+		}
+
+		[DefaultValue("dd.MM.yyyy")]
+		[RefreshProperties(RefreshProperties.Repaint)]
+		public string CustomFormat
+		{
+			get => _customFormat;
+			set
+			{
+				if (value != null)
+				{
+					_customFormat = value;
+					if (_value != null)
+					{
+						dtp.CustomFormat = value;
+					}
+				}
+			}
+		}
+
+		protected virtual void OnValueChanged(EventArgs eventArgs)
+			=> _onValueChanged?.Invoke(this, eventArgs);
+
+		private void Dtp_ValueChanged(object sender, EventArgs e)
+		{
+			if (_reactToValueChange)
+			{
+				Value = dtp.Value;
+			}
+		}
+
+		private void Dtp_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Delete)
+			{
+				Value = null;
+			}
+		}
+
+	}
+}
